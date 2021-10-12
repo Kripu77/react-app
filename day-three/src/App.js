5d48a5d3db9bbf7ce1bd466577cad5e02ce18e30
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-12 19:36:39 +1100

    Update with demonstration

--- day-three/src/App.js
+++ day-three/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const App = () => {
+    //slice method returns a new array it bascially removes the selected value in the slice method and only returns the unsxelected value.
+    let users = [ "Kripu", "Äsim", "John", "Nikita", "Ryan"]
+
+console.log(users.slice(1,4));
+
     return (
         <div>
             <h1> This is a test</h1>
+            {}
         </div>
     )
 }
--- day-three/src/Tes.js
+++ day-three/src/Tes.js
@@ -1,1 +1,4 @@
 // splice and slice in JavaScript Array method
+let users = [ "Kripu", "Äsim", "John", "Nikita", "Ryan"]
+
+console.log(users.slice(1,3));
