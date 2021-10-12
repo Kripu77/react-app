0b4cc37458043a5023ff14439d9c92c237b75c01
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-12 19:48:54 +1100

    update with the slice method

--- day-three/src/App.js
+++ day-three/src/App.js
@@ -4,9 +4,8 @@ const App = () => {
     //slice method returns a new array it bascially removes the selected value in the slice method and only returns the unsxelected value.
     let users = [ "Kripu", "Äsim", "John", "Nikita", "Ryan"]
 
-console.log(users.slice(1,3));
-//we can also do 
-console.log(users.slice(1))
+
+
 //this will slice off only the 1st value which is the 0 index based value
 
     return (
