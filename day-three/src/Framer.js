674fb0fdaa725cee6ac33b247663372256dba061
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-16 00:27:37 +1100

    update

--- day-three/src/Framer.js
+++ day-three/src/Framer.js
@@ -36,12 +36,11 @@ const Framer = () => {
     <img className="demo-img"src={'https://particularaudience.com/static/cebf62198a4742e25913d31c692c6790/e04ce/6f1a76dc-bead-45d2-989e-71895c0ac17d_PA%2BPlatform%2BDiscs.png'}/>
     </motion.div>
     </div>
-    <div className='parent'>
+    <div className='second-parent'>
     <div className='first-child'> 
     <h1> Dear user I'm a framer</h1>
     <img className="demo-img"src={'https://particularaudience.com/static/a272a2fd6d349297d24ff2b816cd9139/e04ce/e09aaf1c-53fe-48f7-8d8b-8397d1820c73_PA%2BRetail%2BAI.png'}/>
     </div>
-
     <motion.div className='second-child' animate={{ 
         y: 15,
     opacity:7 }}
@@ -59,6 +58,7 @@ const Framer = () => {
     
     
     </motion.div>
+
     </LazyLoad>
      </div>
         </div>
