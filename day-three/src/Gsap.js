d2e8990196fa7048fcace325cc2e1c7474f4119e
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-15 21:00:13 +1100

    GSAP setup

---
+++ day-three/src/Gsap.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {gsap} from "gsap";
+
+const Gsap = () => {
+    return (
+        <div className='app'>
+            <div className='box'> Hello this is my first GSAP</div>
+            
+        </div>
+    )
+}
+
+export default Gsap
