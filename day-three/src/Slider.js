af5ff41ba1915e630417b1fe5c0755821c1eec78
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-21 17:39:45 +1100

    Component Update

--- day-three/src/Slider.js
+++ day-three/src/Slider.js
@@ -3,17 +3,22 @@ import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import gates from "./Images/billgates.jpg"
 import zuker from "./Images/elonmusk.jfif"
-import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
+ 
+const AutoplaySlider = withAutoplay(AwesomeSlider);
  
 const Slider = () => {
-    return<AwesomeSlider animation="cubeAnimation">
+    return <AutoplaySlider     play={true}
+    cancelOnInteraction={false} // should stop playing on user interaction
+    interval={2000}>
     <div>
         <img src={gates}/>
     </div>
     <div> <img src={'https://tse1.mm.bing.net/th?id=OIP.RiuE4kmcPGEHhmO7HNy0bwHaDt&pid=Api&P=0&w=381&h=191'}/></div>
     <div style={{backgroundColor:"orange"}}>3</div>
     <div style={{backgroundColor:"lightgreen"}}>4</div>
-  </AwesomeSlider>
+  </AutoplaySlider>
 }
 
 export default Slider
