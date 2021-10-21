edfbef25cab0256581ee3bd780f509bed2ce5173
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-21 16:48:34 +1100

    Demo completed

--- day-three/src/Modalc.js
+++ day-three/src/Modalc.js
@@ -1,18 +1,32 @@
 import React,{useState} from 'react'
 import Modal from 'react-modal'
 
+//to get rid of react complaining please have this fn handy
+Modal.setAppElement('#root')
 const Modalc = () => {
-    const[isModalOpen, setIsModalOpen] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false)
     return (
         <div>
-           <Modal isOpen={isModalOpen} onRequestClose={()=>{setIsModalOpen(false)}}>
-               <h1>Modal Title</h1>
-               <p> Modal Bodyd</p>
-               <button onClick={()=>setIsModalOpen(false)}>Close</button>
-           </Modal>
-
-           <h1> I have a modal component would you like to see?</h1>
-           <button onClick={()=>{setIsModalOpen(true)}}> Open</button>
+            <Modal isOpen={isModalOpen} onRequestClose={()=>setIsModalOpen(false)}
+            style={
+             {
+                     
+                    content:{
+                        color:'gray',
+                        backgroundColor:'black',
+                        margin: '20vh auto',
+                        width: '20vw',
+                        height:'20vh'
+                    }
+                }
+            } >
+                <h1> Modal is open now how did you find me?</h1>
+                <button onClick={()=>{setIsModalOpen(false)}}>Close</button>
+            </Modal>
+           <h1> I am modal example please click me to check demo</h1> 
+           <button onClick={()=>setIsModalOpen(true)}>
+               Open
+           </button>
         </div>
     )
 }
